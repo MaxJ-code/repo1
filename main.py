a030901b4b77a69d3989143caf33d8ca240581a8
MaxJ-code <maxjprivat@gmail.com>
2024-07-04 18:06:46 +0200

    'prevers1'

---
+++ main.py
@@ -0,0 +1,36 @@
+class Part:
+  def __init__(self, name, x, y, z):
+    self.name = name
+    self.x = x
+    self.y = y
+    self.z = z
+
+name = "test"
+x1 = 0
+y1 = 0
+z1 = 0
+p0 = None
+p1 = None
+p2 = None
+p3 = None
+list1 = [p0, p1, p2, p3]
+listout = []
+
+
+#System nach dem Eintrag im Notizbuch
+
+
+for item in list1:
+  for itemx in list1:
+    for
+  item = Part(name, x1, y1, z1)
+  
+  y1 += 1
+  z1 += 1
+  listout.append(item)
+for out in listout:
+  print("Name: ", out.name)
+  print("x: ", out.x)
+  print("y: ", out.y)
+  print("z: ", out.z)
+  
